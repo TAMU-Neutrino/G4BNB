a40ad0ac7d85495dfe475a49abcd2dee94fb1c60
Austin Schneider <hogenshpogen@gmail.com>
2025-12-21 22:07:12 -0600

    Comment duplicate line

--- src/NuBeam.cc
+++ src/NuBeam.cc
@@ -53,7 +53,7 @@ int main(int argc,char** argv)
   
 #ifdef G4VIS_USE
   // visualization manager
-  G4VisManager* visManager = new G4VisExecutive;
+  // G4VisManager* visManager = new G4VisExecutive;
   // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
   G4VisManager* visManager = new G4VisExecutive("Quiet");
   visManager->Initialize();
